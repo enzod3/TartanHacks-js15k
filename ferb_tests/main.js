64c4c522be915a18d484c7036af21fd5304057c0
ferb-ferb <ajferber@gmail.com>
2026-02-06 19:19:39 -0500

    merge branches

--- ferb_tests/main.js
+++ ferb_tests/main.js
@@ -9,7 +9,15 @@ resize();
 addEventListener("resize", resize);
 
 W.reset(c);
-W.camera({x:0,y:0,z:0});
+
+// Move camera UP (y: 2), BACK (z: 3), and TILT DOWN (rx: -30)
+W.camera({ x: 0, y: 2, z: 3, rx: -30 }); 
+
 W.clearColor("#111");
 W.ambient(0.6);
-W.plane({size:150,b:"3d2",z:-100,y:-75,ns:1});
+
+W.plane({size: 150,
+  x: 0, y: 0, z: 0,
+  rx: -90,     // Rotates it flat to be a floor
+  b: "f00"     // Make it RED so you can definitely see it
+});
